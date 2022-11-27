35d05e6144cd5a18cd97ee55806c4234a8803a38
unknown <adsa_sk8@hotmail.com>
2022-11-27 19:08:29 -0300

    group table creation

--- nlw/js/main.js
+++ nlw/js/main.js
@@ -4,6 +4,7 @@ function createGame(player1, hour, player2) {
       <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}" />
       <strong>${hour}</strong>
       <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}" />
+      
     </li>
   `
 }
